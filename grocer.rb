35b674299f08d46624b5e0f74d25e824200a75b7
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-10 09:24:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,14 +1,38 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  collection.length.times do |index|
+    if collection[index][:item] == name
+      return collection[index]
+    end 
+  end
+  return nil
 end
 
+def return_index(name, collection)
+  collection.length.times do |index|
+    if collection[index][:item] == name
+      return index 
+    end 
+  end
+end 
+
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  #create empty cart, which will be returned
+  fixed_cart = []
+  cart.length.times do |index| #iterate through the argument
+    #call the find_item method and atrib the return to a var 
+    item = find_item_by_name_in_collection(cart[index][:item], fixed_cart)
+    if !item #if the returned value is nil means that we don't have the item yet
+      #create a new entry and add a value of 1 to :count
+      fixed_cart.push(cart[index])
+      fixed_cart[-1][:count] = 1
+    else #which means that the item is already in the cart 
+      #call method return_index to find out in what index the item is 
+      index_where_item_is = return_index(cart[index][:item], fixed_cart)
+      #add 1 to count
+      fixed_cart[index_where_item_is][:count] += 1
+    end
+  end 
+  fixed_cart
 end
 
 def apply_coupons(cart, coupons)
